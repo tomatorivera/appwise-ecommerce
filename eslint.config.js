87e178cd8f41a873718e00ede521de8aa93eb737
tomatorivera <tomatorivera@gmail.com>
2026-05-29 16:43:34 -0300

    chore: instalar y configurar prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -5,7 +6,8 @@ import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import jsxA11y from "eslint-plugin-jsx-a11y";
-import { defineConfig } from "eslint/config";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default defineConfig([
   {
@@ -14,11 +16,13 @@ export default defineConfig([
       js.configs.recommended,
       react.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite
+      reactRefresh.configs.vite,
+      prettierConfig,
     ],
     plugins: {
       react,
-      "jsx-a11y": jsxA11y
+      "jsx-a11y": jsxA11y,
+      prettierPlugin,
     },
     languageOptions: {
       globals: globals.browser,
@@ -41,6 +45,7 @@ export default defineConfig([
       "no-unused-vars": "warn",
       "no-console": "warn",
       "jsx-a11y/alt-text": "warn",
+      "prettier/prettier": "off",
     },
   },
   tseslint.configs.recommended,
