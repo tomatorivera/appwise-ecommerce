8dd1b86470b5c905e8d1417537d2a85803f402eb
tomatorivera <tomatorivera@gmail.com>
2026-05-29 15:57:18 -0300

    chore: instalar y configurar tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <h1>Hola mundo desde e-commerce</h1>
+    <div className="h-screen w-screen bg-blue-500 grid place-items-center">
+      <h1 className="text-white">Hola mundo desde e-commerce</h1>
+    </div>
   )
 }
 
