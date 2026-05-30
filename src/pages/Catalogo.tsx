b2e486e2bec4bb4a06f974e171477cb481c962d8
tomatorivera <tomatorivera@gmail.com>
2026-05-30 20:27:00 -0300

    feat(catalogo): renderizar catalogo de productos con un componente que use una lista genérica

--- src/pages/Catalogo.tsx
+++ src/pages/Catalogo.tsx
@@ -1,3 +1,4 @@
+import ItemsGrid from '../components/ItemsGrid'
 import ProductCard from '../components/product/ProductCard'
 import { useProductos } from '../hooks/useProductos'
 
@@ -8,11 +9,12 @@ const Catalogo = () => {
     <main className="max-w-7xl mx-auto p-4">
       <h1 className="font-bold text-4xl">Catálogo de productos</h1>
 
-      <section className="py-5 grid grid-cols-1 gap-6 place-items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {productos.map((p) => {
-          return <ProductCard key={p.id} producto={p} />
-        })}
-      </section>
+      <ItemsGrid
+        items={productos}
+        renderItem={(producto) => (
+          <ProductCard key={producto.id} producto={producto} />
+        )}
+      />
     </main>
   )
 }
