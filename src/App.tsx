035b42543e579f54cebfb306b4852552a768b727
tomatorivera <tomatorivera@gmail.com>
2026-05-30 20:30:35 -0300

    feat: implementar enlace desde App hasta catálogo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom'
+
 function App() {
+  // Todo: reemplazar con homepage
+
   return (
-    <div className="h-screen w-screen bg-blue-500 grid place-items-center">
+    <div className="h-screen w-screen bg-blue-500 flex flex-col justify-center items-center">
       <h1 className="text-white">Hola mundo desde e-commerce</h1>
+
+      <Link to="/catalogo" className="text-white underline font-semibold">
+        Ir al catálogo →
+      </Link>
     </div>
   )
 }
