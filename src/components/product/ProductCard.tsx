30e02f7ad0212912d97ebc0c844a3b64b64ca862
tomatorivera <tomatorivera@gmail.com>
2026-06-10 18:42:36 -0300

    refactor: modificar card de producto para que el link abarque todo el contenido

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -7,25 +7,38 @@ interface ProductCardProps {
 
 const ProductCard = ({ producto }: ProductCardProps) => {
   return (
-    <article className="overflow-hidden border border-gray-300 bg-white w-full h-full">
-      <img
-        src={producto.thumbnail}
-        alt={producto.title}
-        className="h-70 w-full object-cover"
-      />
+    <Link
+      to={`/producto/${producto.id}`}
+      className="mt-auto text-sm font-medium w-full h-full"
+    >
+      <article className="overflow-hidden border border-gray-300 bg-white w-full h-full flex flex-col">
+        <img
+          src={producto.thumbnail}
+          alt={producto.title}
+          className="h-70 w-full object-cover"
+        />
 
-      <section className="flex flex-col gap-3 p-4">
-        <h2 className="line-clamp-2 text-lg font-semibold">{producto.title}</h2>
-        <p className="text-2xl font-bold text-green-600">${producto.price}</p>
+        <section className="flex flex-col gap-3 p-4 justify-between flex-1">
+          <div>
+            <h2 className="line-clamp-2 text-lg font-semibold">
+              {producto.title}
+            </h2>
+            <p className="text-2xl font-bold text-green-600">
+              ${producto.price}
+            </p>
+          </div>
 
-        <Link
-          to={`/producto/${producto.id}`}
-          className="mt-auto text-sm font-medium text-blue-600 hover:underline"
-        >
-          Leer más →
-        </Link>
-      </section>
-    </article>
+          <div>
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-xl cursor-pointer"
+              // onClick={(e: React.MouseEvent<HTMLButtonElement>) => handleAddItemToCart(producto.title, e)}
+            >
+              Agregar al carrito
+            </button>
+          </div>
+        </section>
+      </article>
+    </Link>
   )
 }
 
