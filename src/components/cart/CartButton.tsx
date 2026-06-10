bc10cb852f14a7e823fccc8ff27346a4e39c48a4
tomatorivera <tomatorivera@gmail.com>
2026-06-10 17:52:04 -0300

    feat(ticket-04): terminar implementación de CartButton utilizando useCarrito

--- src/components/cart/CartButton.tsx
+++ src/components/cart/CartButton.tsx
@@ -1,5 +1,6 @@
-import { ShoppingCart } from 'react-feather'
+import { ShoppingCart, X } from 'react-feather'
 import { useToggle } from '../../hooks/useToggle'
+import { useCarrito } from '../../features/carrito/useCarrito'
 
 const CartButton = () => {
   const {
@@ -7,6 +8,12 @@ const CartButton = () => {
     activate: openDropdown,
     deactivate: closeDropdown,
   } = useToggle()
+
+  const { items, quitarItem, totalItems, totalPrecio } = useCarrito()
+
+  const handleRemoveFromCart = (productId: number) => {
+    quitarItem(productId)
+  }
 
   return (
     <div className="relative">
@@ -19,56 +26,63 @@ const CartButton = () => {
           className="text-white hover:text-emerald-100 transition-colors"
         />
 
-        {/* Todo: actualizar la cantidad dinámicamente */}
         <span className="absolute -top-3.75 -right-4.25 bg-white text-sm rounded-full p-0.75 text-emerald-700">
-          10
+          {totalItems}
         </span>
       </button>
 
       {isOpen && (
         <div className="absolute bg-white -right-50 w-80 top-10 border border-gray-300 shadow-xl z-50 md:-right-3 md:top-6 md:mt-3">
           <ul className="max-h-96 overflow-y-auto divide-y divide-gray-100">
-            <li className="flex gap-3 p-4">
-              <img
-                src="https://via.placeholder.com/60"
-                alt="Producto"
-                className="h-15 w-15 rounded object-cover"
-              />
+            {items.length > 0 ? (
+              items.map((producto) => {
+                return (
+                  <li className="flex gap-3 p-4 pb-0">
+                    <img
+                      src={producto.thumbnail}
+                      alt={`Imagen del producto: ${producto.title}`}
+                      className="h-15 w-15 rounded object-cover"
+                    />
 
-              <div className="flex-1">
-                <h3 className="line-clamp-2 text-sm font-medium">
-                  Producto de ejemplo
-                </h3>
+                    <div className="relative flex-1">
+                      <h3 className="line-clamp-2 text-sm font-medium pe-2">
+                        {producto.title}
+                      </h3>
 
-                <p className="text-sm text-gray-500">Cantidad: 2</p>
+                      <p className="text-sm text-gray-500">
+                        Cantidad: {producto.cantidad}
+                      </p>
+                      <p className="text-sm text-gray-500">
+                        Precio unitario: ${producto.price}
+                      </p>
+                      <p className="font-semibold text-emerald-700">
+                        ${(producto.price * producto.cantidad).toFixed(2)}
+                      </p>
 
-                <p className="font-semibold text-emerald-700">$15.000</p>
-              </div>
-            </li>
-
-            <li className="flex gap-3 p-4">
-              <img
-                src="https://via.placeholder.com/60"
-                alt="Producto"
-                className="h-15 w-15 rounded object-cover"
-              />
-
-              <div className="flex-1">
-                <h3 className="line-clamp-2 text-sm font-medium">
-                  Otro producto
-                </h3>
-
-                <p className="text-sm text-gray-500">Cantidad: 1</p>
-
-                <p className="font-semibold text-emerald-700">$8.500</p>
-              </div>
-            </li>
+                      <button
+                        className="cursor-pointer"
+                        onClick={() => handleRemoveFromCart(producto.id)}
+                      >
+                        <X
+                          className="absolute top-0 right-0 text-red-600"
+                          size={20}
+                        />
+                      </button>
+                    </div>
+                  </li>
+                )
+              })
+            ) : (
+              <p className="text-gray-500 text-center py-5">
+                No hay items en el carrito
+              </p>
+            )}
           </ul>
 
           <div className="border-t border-gray-200 p-4">
             <div className="mb-3 flex justify-between font-semibold">
               <span>Total</span>
-              <span>$23.500</span>
+              <span>${totalPrecio.toFixed(2)}</span>
             </div>
 
             <button className="w-full rounded bg-emerald-600 py-2 text-white hover:bg-emerald-700 transition-colors cursor-pointer">
