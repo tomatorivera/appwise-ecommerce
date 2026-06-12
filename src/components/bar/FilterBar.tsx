454c3b078ca556f416c08f768f630c362b8f9a79
tomatorivera <tomatorivera@gmail.com>
2026-06-11 23:40:42 -0300

    feat(homepage): implementar useCategorias para traer categorias de productos desde la API

--- src/components/bar/FilterBar.tsx
+++ src/components/bar/FilterBar.tsx
@@ -1,29 +1,45 @@
+import { useCategorias } from '../../hooks/useCategorias'
+
 interface Props {
   className?: string
 }
 
 const FilterBar = ({ className = '' }: Props) => {
+  const responseCategorias = useCategorias()
+
+  if (responseCategorias.status === 'error')
+    console.error(responseCategorias.error)
+
   return (
     <aside className={`bg-gray-100 p-3 ${className}`}>
       <h2 className="text-xl font-bold">Filtros</h2>
-
-      {/* Todo: traer categorías de la api */}
       <section className="my-4">
         <h3 className="text-md font-semibold">Categorías</h3>
-        <ul className="py-2">
-          <li className="flex items-center gap-2">
-            <input type="checkbox" name="cat1" id="cat1" />
-            <label htmlFor="cat1">Categoría 1</label>
-          </li>
-          <li className="flex items-center gap-2">
-            <input type="checkbox" name="cat2" id="cat2" />
-            <label htmlFor="cat2">Categoría 2</label>
-          </li>
-          <li className="flex items-center gap-2">
-            <input type="checkbox" name="cat3" id="cat3" />
-            <label htmlFor="cat3">Categoría 1</label>
-          </li>
-        </ul>
+
+        {responseCategorias.status === 'idle' && (
+          <p className="py-2">Cargando categorias...</p>
+        )}
+
+        {responseCategorias.status === 'error' && (
+          <p className="py-2">Error al cargar las categorías</p>
+        )}
+
+        {responseCategorias.status === 'success' && (
+          <ul className="py-2">
+            {responseCategorias.data.map((category, index) => {
+              return (
+                <li className="flex items-center gap-2">
+                  <input
+                    type="checkbox"
+                    name={`cat-${index}`}
+                    id={`cat-${index}`}
+                  />
+                  <label htmlFor={`cat-${index}`}>{category}</label>
+                </li>
+              )
+            })}
+          </ul>
+        )}
       </section>
     </aside>
   )
